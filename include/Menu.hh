f548c9a2a2cd818b2c3a25ccac0627849982fc7d
Freddy <obrecht@phys.uconn.edu>
2016-01-29 11:01:21 -0500

    Currently, there is a bug when the buttons are initialized

--- include/Menu.hh
+++ include/Menu.hh
@@ -13,8 +13,8 @@ private:
   std::vector<sf::VertexArray> colorsvec;
 
   // The Slider Menu
-  sf::RectangleShape slider,bg;
-  std::vector<sf::RectangleShape> sliders, bg_vec;
+  sf::RectangleShape slider,bg,rgb_bg;
+  std::vector<sf::RectangleShape> sliders, bg_vec, RGB_bg_vec;
 
   sf::VertexArray slider_line, shade;
   std::vector<sf::VertexArray> slider_line_vec,shade_vec, slider_shade_vec;
@@ -22,22 +22,46 @@ private:
   std::vector<sf::RectangleShape>::iterator slide_it;
   std::vector<sf::VertexArray>::iterator sit;
 
-  int R,G,B,R2;
-  bool update;
+  // controls RGB circles
+  std::map<int,std::vector<sf::CircleShape> > rgb_map;
+  std::map<int,std::vector<sf::CircleShape> >::iterator rgb_map_it;
+  std::vector<sf::CircleShape> rgb_vec;
+  std::vector<sf::CircleShape>::iterator rgb_vec_it;
+
+  // controls the "button" feature
+  std::map<int,sf::CircleShape> buttons;
+  std::map<int,sf::CircleShape>::iterator bit;
+
+  // Initializations for top left (TL), top right (TR), etc...
+  sf::Color TL,TR,BR,BL;
+  int TL_mag,TR_mag,BR_mag,BL_mag;
+  bool update; // controls UpdateSliders, we only want an update once
+               // the mouse is released
+  bool update_button;
+
+  //Boarder
+  sf::RectangleShape ceiling, left, right, bottom;
+  std::vector<sf::RectangleShape> boarder;
+
 
 public:
   Menu(float,float);
   ~Menu() {};
   void draw(sf::RenderTarget&, sf::RenderStates) const;
-
+  void DrawSlider(sf::RenderTarget&) const;
+  void DrawBoarder(sf::RenderTarget&) const;
+  
   void MakeMenu();
-  void MakeVertexColors();
+  void MakeVertexColors(sf::Color,sf::Color,sf::Color,sf::Color);
+  void UpdateVertexColors(sf::Color,int);
 
   void MakeSlider(float,float,int,float,sf::Color, sf::RectangleShape);
-  void DrawSlider(sf::RenderTarget&) const;
   void UpdateSliders(sf::Vector2f);
   void SetSliderValue();
-
+  void ResetSlider();
+  void MakeRGB(sf::Color);
+  void UpdateRGB(sf::Vector2f);
   void ShadeRectangle(sf::Vector2f,sf::Vector2f,sf::Color,float,bool);
+  void MakeBoarder(float,float,float,sf::Color);
 };
 #endif
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -6,15 +6,22 @@ Menu::Menu(float x, float y) {
   displayx = x;
   displayy = y;
 
+  // Makes White Box on LHS
   MakeMenu();
 
+  // Initial the VertexArray QUAD, initialize to full intensity
+  // RGBR2 control initial RGB values from 0-255
   colors = sf::VertexArray(sf::Quads, 4);
-
-  R = 0;
-  G = 100;
-  B = 0;
-  R2 = 100;
-  MakeVertexColors();
+  TL_mag = 255;
+  TR_mag = 255;
+  BR_mag = 255;
+  BL_mag = 255;
+  // runs CW
+  TL = sf::Color::Red;
+  TR = sf::Color::Red;
+  BR = sf::Color::Blue;
+  BL = sf::Color::Green;
+  MakeVertexColors(TL,TR,BR,BL);
 
   slider_line = sf::VertexArray(sf::LinesStrip,2);
   shade = sf::VertexArray(sf::Quads, 4);
@@ -24,48 +31,42 @@ Menu::Menu(float x, float y) {
   slider.setOrigin( sliderx/2.0, slidery/2.0 );
   slider.setFillColor( sf::Color(230,230,230) );
 
-  MakeSlider( displayx, displayy, R, 0.0, sf::Color::Red, slider);
-  MakeSlider( displayx, displayy, G, 150.0, sf::Color::Green, slider);
-  MakeSlider( displayx, displayy, B, 300.0, sf::Color::Blue, slider);
-  MakeSlider( displayx, displayy, R2, 450.0, sf::Color::Red, slider);
+  MakeSlider( displayx, displayy, TL_mag, 0.0, sf::Color(80,80,80), slider);
+  MakeSlider( displayx, displayy, TR_mag, 150.0, sf::Color(80,80,80), slider);
+  MakeSlider( displayx, displayy, BR_mag, 300.0, sf::Color(80,80,80), slider);
+  MakeSlider( displayx, displayy, BL_mag, 450.0, sf::Color(80,80,80), slider);
 
+  MakeRGB( sf::Color(150,150,150) );
+
+  // Controls UpdateSliders
   update = false;
-}
-
-void Menu::MakeVertexColors() {
-  sf::FloatRect tmp = menu.getLocalBounds();
-  colors[0].position = sf::Vector2f( tmp.left + tmp.width, tmp.top );
-  colors[1].position = sf::Vector2f( tmp.left + tmp.width, tmp.top + displayy ); 
-  colors[2].position = sf::Vector2f( displayx, displayy );
-  colors[3].position = sf::Vector2f( displayx, 0.0 );
-  colors[0].color = sf::Color(R,0,0);
-  colors[1].color = sf::Color(0,G,0);
-  colors[2].color = sf::Color(0,0,B);
-  colors[3].color = sf::Color(R2,0,0);
-  colorsvec.push_back( colors );
+  update_button = true;
 }
 
 void Menu::MakeMenu() {
   sf::Vector2f size( displayx/4.0, displayy );
   menu.setSize( size );
   menu.setOrigin( size.x/2.0, size.y/2.0 );
-  menu.setFillColor(sf::Color::White);
+  menu.setFillColor(sf::Color(235,235,235));
   menu.setPosition( size.x/2.0, size.y/2.0 );
+  MakeBoarder(size.x,size.y,6.0,sf::Color(80,80,80));
 }
 
-void Menu::draw( sf::RenderTarget& target, sf::RenderStates ) const {
-  target.draw( menu );
-  std::vector<sf::VertexArray>::const_iterator cit;
-  for(cit=colorsvec.begin(); cit!=colorsvec.end(); cit++){
-    target.draw(*cit);
-  }
-  std::vector<sf::RectangleShape>::const_iterator rit;
-  for(rit=bg_vec.begin(); rit!=bg_vec.end(); rit++){
-    target.draw(*rit);
-  }
-  DrawSlider( target );
+void Menu::MakeVertexColors(sf::Color TL,sf::Color BL,sf::Color BR,sf::Color TR) {
+  sf::FloatRect tmp = menu.getLocalBounds();
+  colors[0].position = sf::Vector2f( tmp.left + tmp.width, tmp.top );
+  colors[1].position = sf::Vector2f( tmp.left + tmp.width, tmp.top + displayy ); 
+  colors[2].position = sf::Vector2f( displayx, displayy );
+  colors[3].position = sf::Vector2f( displayx, 0.0 );
+  colors[0].color = TL;
+  colors[1].color = TR;
+  colors[2].color = BR;
+  colors[3].color = BL;
+  colorsvec.push_back( colors );
 }
-
+////////////////////////////////////////////////////////
+//                     SLIDERS                        //
+////////////////////////////////////////////////////////
 void Menu::MakeSlider(float x, float y, int input, float offset, sf::Color color, sf::RectangleShape slide ) {
   sf::FloatRect tmp = menu.getLocalBounds();
   float centerX = tmp.left+tmp.width;
@@ -107,24 +108,309 @@ void Menu::MakeSlider(float x, float y, int input, float offset, sf::Color color
   float initial_offset = float(input) / 255.0;
   slide.setPosition( sf::Vector2f(start.x+initial_offset*slider_mag, start.y) );
   sliders.push_back( slide );
-  // Shade the Slider
-  ShadeRectangle(slide.getPosition(), slide.getSize(), sf::Color(80,80,80), 2.0,true);
+  // Shade the Slider ***NEEDS UPDATING*** - how do I get vertex array to move 
+  // with Mouse Position???
+  //ShadeRectangle(slide.getPosition(), slide.getSize(), sf::Color(80,80,80), 2.0, true);
 }
 
+void Menu::UpdateSliders(sf::Vector2f mouseP){
+
+  if( sf::Mouse::isButtonPressed(sf::Mouse::Left) ) {
+    update = true;
+    sf::Vector2f position;
+    for( int i=0; i<slider_line_vec.size(); i++ ){
+      // check to see if mouse if close
+      position = sliders[i].getPosition(); 
+      sf::FloatRect bounds = slider_line_vec[i].getBounds();
+      sf::Vector2f D = mouseP - position;
+      float D_2 = pow(D.x,2) + pow(D.y,2);
+
+      if( D_2 <= 0.004*( pow(position.x,2)+pow(position.y,2) ) ) {
+	if( mouseP.x <= bounds.left ) {
+	  sliders[i].setPosition( bounds.left, bounds.top );
+	}
+	if( mouseP.x >= (bounds.left+bounds.width) ) {
+	  sliders[i].setPosition( bounds.left + bounds.width, bounds.top );
+	}
+	if( mouseP.x > bounds.left && mouseP.x < (bounds.left+bounds.width) )  {
+	  sliders[i].setPosition( mouseP.x, position.y );  
+	}
+      }
+      
+    }
+  }
+  else {
+    if( update ) {
+      //SetSliderValue();
+      update = false;
+    }
+  }
+}
+
+void Menu::SetSliderValue(){
+  for( int i=0; i<slider_line_vec.size(); i++ ){
+    sf::FloatRect bounds = slider_line_vec[i].getBounds();
+    sf::Vector2f sliderPos = sliders[i].getPosition();
+    
+    float RGB_value = (sliderPos.x - bounds.left)/200.0*255.0;
+    int newRGB = int(RGB_value);
+    sf::FloatRect tmp = menu.getLocalBounds();
+    colorsvec.clear();
+    colors[0].position = sf::Vector2f( tmp.left + tmp.width, tmp.top );
+    colors[1].position = sf::Vector2f( tmp.left + tmp.width, tmp.top + displayy ); 
+    colors[2].position = sf::Vector2f( displayx, displayy );
+    colors[3].position = sf::Vector2f( displayx, 0.0 );
+    // 0 = TL
+    // 1 = TR
+    // 2 = BR
+    // 3 = BL
+  
+    if(newRGB>=0){ 
+      switch(i){
+      case 0 :
+	TL_mag = newRGB;
+	break;
+      case 1 :
+	TR_mag = newRGB;
+	break;
+      case 2 : 
+	BR_mag = newRGB;
+	break;
+      case 3 : 
+	BL_mag = newRGB;
+	break;
+      default : 
+	break;
+      }
+      
+      if( buttons[0].getFillColor() == sf::Color::Red ) {
+	TL = sf::Color(TL_mag,0,0);
+      }
+      if( buttons[0].getFillColor() == sf::Color::Green ) {
+	TL = sf::Color(0,TL_mag,0);
+      }
+      if( buttons[0].getFillColor() == sf::Color::Blue ) {
+	TL = sf::Color(0,0,TL_mag);
+      }
+      if( buttons[1].getFillColor() == sf::Color::Red ) {
+	TR = sf::Color(TR_mag,0,0);
+      }
+      if( buttons[1].getFillColor() == sf::Color::Green ) {
+	TR = sf::Color(0,TR_mag,0);
+      }
+      if( buttons[1].getFillColor() == sf::Color::Blue ) {
+	TR = sf::Color(0,0,TR_mag);
+      }
+      if( buttons[2].getFillColor() == sf::Color::Red ) {
+	BR = sf::Color(BR_mag,0,0);
+      }
+      if( buttons[2].getFillColor() == sf::Color::Green ) {
+	BR = sf::Color(0,BR_mag,0);
+      }
+      if( buttons[2].getFillColor() == sf::Color::Blue ) {
+	BR = sf::Color(0,0,BR_mag);
+      }
+      if( buttons[3].getFillColor() == sf::Color::Red ) {
+	BL = sf::Color(BL_mag,0,0);
+      }
+      if( buttons[3].getFillColor() == sf::Color::Green ) {
+	BL = sf::Color(0,BL_mag,0);
+      }
+      if( buttons[3].getFillColor() == sf::Color::Blue ) {
+	BL = sf::Color(0,0,BL_mag);
+      }	
+      colors[0].color = TL;
+      colors[1].color = TR;
+      colors[2].color = BR;
+      colors[3].color = TL;
+      colorsvec.push_back( colors );
+    }   
+  }
+}
+////////////////////////////////////////////////////////
+//                     BUTTONS                        //
+////////////////////////////////////////////////////////
+void Menu::MakeRGB(sf::Color) {
+  // Has to be called AFTER MakeSlider(), therefore
+  // check to see if it is empty to avoid issues
+
+  if(slider_line_vec.size()>0){
+    for( int i=0; i<slider_line_vec.size(); i++ ){
+      sf::FloatRect bounds = slider_line_vec[i].getBounds();
+      sf::RectangleShape RGB_bg;
+      sf::Vector2f size( (bounds.left + bounds.width)/2.0, sliders[i].getLocalBounds().height + 15.0); 
+      RGB_bg.setSize( size );
+      RGB_bg.setOrigin( size.x/2.0, size.y/2.0 );
+      RGB_bg.setFillColor( sf::Color(150,150,150) );
+      RGB_bg.setPosition( bounds.left + 0.5*bounds.width, bounds.top + RGB_bg.getSize().y + 10.0);
+      RGB_bg_vec.push_back( RGB_bg );
+      ShadeRectangle( RGB_bg.getPosition(), RGB_bg.getSize(), sf::Color(80,80,80), 3.0, false );
+
+      // Now Make the RGB Interface
+      float radius = RGB_bg.getSize().y/3.0;
+      float thickness = 1.0;
+      rgb_vec.clear();
+
+      // Make the RGB Button
+      sf::CircleShape button( radius - 4.0 );
+      sf::FloatRect buttontmp = button.getLocalBounds();
+      button.setOrigin( buttontmp.width/2.0, buttontmp.height/2.0 );
+      // Need to initialize our buttons:
+      switch(i) {
+      case 0 : button.setFillColor(TL);
+	break;
+      case 1 : button.setFillColor(BL);
+	break;
+      case 2 : button.setFillColor(BR);
+	break;
+      case 3 : button.setFillColor(TL);
+	break;
+      default : 
+	break;
+      }
+
+      for( int k=1; k<=3; k++ ) {
+	sf::CircleShape rgb_circle( radius );
+	rgb_circle.setOutlineThickness(-thickness);
+	sf::FloatRect circlebounds = rgb_circle.getLocalBounds();
+	rgb_circle.setOrigin( circlebounds.width/2.0, circlebounds.height/2.0 );
+	rgb_circle.setPosition( RGB_bg.getPosition().x - RGB_bg.getSize().x/2.0 + k*(RGB_bg.getSize().x)/3.0 - 2*radius, RGB_bg.getPosition().y );
+
+	switch( k ) {
+	case 1 : rgb_circle.setOutlineColor( sf::Color::Red );
+	  break;
+	case 2 : rgb_circle.setOutlineColor( sf::Color::Green );
+	  break;
+	case 3 : rgb_circle.setOutlineColor( sf::Color::Blue );
+	  break;
+	default : 
+	  break;
+	}
+	// Check colors, and place our Button
+	if( rgb_circle.getOutlineColor() == button.getFillColor() ) {
+	  button.setPosition(RGB_bg.getPosition().x - RGB_bg.getSize().x/2.0 + k*(RGB_bg.getSize().x)/3.0 - 2*radius, RGB_bg.getPosition().y);
+	}
+	rgb_vec.push_back( rgb_circle );
+      }
+      rgb_map[i] = rgb_vec;
+      buttons[i] = button;
+    }
+  }
+  else {
+    std::cout << "slider_line_vec is empty!" << std::cerr;
+  }
+}
+
+void Menu::UpdateRGB(sf::Vector2f mouseP) {
+  if( sf::Mouse::isButtonPressed(sf::Mouse::Left) ) {
+    if( update_button ) {
+      sf::Vector2f position;
+      for( rgb_map_it = rgb_map.begin(); rgb_map_it != rgb_map.end(); rgb_map_it++ ) {
+	for( rgb_vec_it = (rgb_map_it->second).begin(); 
+	     rgb_vec_it != (rgb_map_it->second).end(); rgb_vec_it++ ) {
+	  position = rgb_vec_it->getPosition();
+	  sf::Vector2f D = mouseP - position;
+	  float D_2 = pow(D.x,2) + pow(D.y,2);
+	  if( D_2 <= 0.002*( pow(position.x,2)+pow(position.y,2) ) ) {
+	    buttons[rgb_map_it->first].setFillColor(rgb_vec_it->getOutlineColor());
+	    buttons[rgb_map_it->first].setPosition(rgb_vec_it->getPosition());
+	    update_button = false;
+	    // send color and row # (or TL,TR,BR,BL)
+	    UpdateVertexColors(rgb_vec_it->getOutlineColor(),rgb_map_it->first);
+	    std::cout << int(rgb_vec_it->getOutlineColor().r) << " " << rgb_map_it->first << std::endl;
+	  }
+	}
+      }
+    }
+  }
+  else 
+    update_button = true;
+}
+
+void Menu::UpdateVertexColors(sf::Color newcolor, int corner) {
+  switch(corner){
+  case 0 :
+    TL = newcolor;
+    break;
+  case 1 :
+    TR = newcolor;
+    break;
+  case 2 : 
+    BR = newcolor;
+    break;
+  case 3 : 
+    BL = newcolor;
+    break;
+  default : 
+    break;
+  }
+  sf::FloatRect tmp = menu.getLocalBounds();
+  //colorsvec.clear();
+  colors[0].position = sf::Vector2f( tmp.left + tmp.width, tmp.top );
+  colors[1].position = sf::Vector2f( tmp.left + tmp.width, tmp.top + displayy ); 
+  colors[2].position = sf::Vector2f( displayx, displayy );
+  colors[3].position = sf::Vector2f( displayx, 0.0 );
+  colors[0].color = TL;
+  colors[1].color = TR;
+  colors[2].color = BR;
+  colors[3].color = BL;
+  colorsvec.push_back( colors );
+}
+
+////////////////////////////////////////////////////////
+//                        DRAW                        //
+////////////////////////////////////////////////////////
 void Menu::DrawSlider(sf::RenderTarget& targ) const{
   std::vector<sf::VertexArray>::const_iterator cit1;
-  std::vector<sf::RectangleShape>::const_iterator cit;
   for( cit1=slider_line_vec.begin(); cit1!=slider_line_vec.end(); cit1++ ){
     targ.draw(*cit1);
   }
   for( cit1=shade_vec.begin(); cit1!=shade_vec.end(); cit1++ ){
     targ.draw(*cit1);
   }
+  std::vector<sf::RectangleShape>::const_iterator cit;
   for( cit=sliders.begin(); cit!=sliders.end(); cit++ ){
+    targ.draw(*cit);
+  }
+  for( cit=RGB_bg_vec.begin(); cit!=RGB_bg_vec.end(); cit++ ){
+    targ.draw(*cit);
+  }
+  std::map<int,std::vector<sf::CircleShape> >::const_iterator cit2;
+  std::vector<sf::CircleShape>::const_iterator cit3;
+  for( cit2 = rgb_map.begin(); cit2 != rgb_map.end(); cit2++ ){
+    for( cit3 = (cit2->second).begin(); cit3 != (cit2->second).end(); cit3++ ) {
+      targ.draw( *cit3 );
+    }
+  }
+  std::map<int,sf::CircleShape>::const_iterator cit4;
+  for(cit4 = buttons.begin(); cit4 != buttons.end(); cit4++ ){
+    targ.draw(cit4->second);
+  }
+}
+
+void Menu::DrawBoarder(sf::RenderTarget& targ) const{
+  std::vector<sf::RectangleShape>::const_iterator cit;
+  for( cit=boarder.begin(); cit!=boarder.end(); cit++ ){
     targ.draw(*cit);
   }
 }
 
+void Menu::draw( sf::RenderTarget& target, sf::RenderStates ) const {
+  target.draw( menu );
+  std::vector<sf::VertexArray>::const_iterator cit;
+  for(cit=colorsvec.begin(); cit!=colorsvec.end(); cit++){
+    target.draw(*cit);
+  }
+  std::vector<sf::RectangleShape>::const_iterator rit;
+  for(rit=bg_vec.begin(); rit!=bg_vec.end(); rit++){
+    target.draw(*rit);
+  }
+  DrawBoarder( target );
+  DrawSlider( target );
+}
+////////////////////////////////////////////////////////
+//                       SHADE                        //
+////////////////////////////////////////////////////////
 void Menu::ShadeRectangle(sf::Vector2f pos,sf::Vector2f size,sf::Color col,float thickness,bool YorN){
   // TOP
   shade[0].position = sf::Vector2f( pos.x - size.x/2.0, pos.y - size.y/2.0 );
@@ -155,92 +441,29 @@ void Menu::ShadeRectangle(sf::Vector2f pos,sf::Vector2f size,sf::Color col,float
   else shade_vec.push_back( slider_line );
 }  
 
-void Menu::UpdateSliders(sf::Vector2f mouseP){
-
-  if( sf::Mouse::isButtonPressed(sf::Mouse::Left) ) {
-    update = true;
-    sf::Vector2f position;
-    for( int i=0; i<slider_line_vec.size(); i++ ){
-      // check to see if mouse if close
-      position = sliders[i].getPosition(); 
-      sf::FloatRect bounds = slider_line_vec[i].getBounds();
-      sf::Vector2f D = mouseP - position;
-      float D_2 = pow(D.x,2) + pow(D.y,2);
-
-      if( D_2 <= 0.04*( pow(position.x,2)+pow(position.y,2) ) ) {
-	if( mouseP.x <= bounds.left ) {
-	  sliders[i].setPosition( bounds.left, bounds.top );
-	}
-	if( mouseP.x >= (bounds.left+bounds.width) ) {
-	  sliders[i].setPosition( bounds.left + bounds.width, bounds.top );
-	}
-	if( mouseP.x > bounds.left && mouseP.x < (bounds.left+bounds.width) )  {
-	  sliders[i].setPosition( mouseP.x, position.y );  
-	}
-      }
-      
-    }
-  }
-  else {
-    if( update ) {
-      SetSliderValue();
-      update = false;
-    }
-  }
-}
-
-void Menu::SetSliderValue(){
-  for( int i=0; i<slider_line_vec.size(); i++ ){
-    sf::FloatRect bounds = slider_line_vec[i].getBounds();
-    sf::Vector2f sliderPos = sliders[i].getPosition();
-    
-    float RGB_value = (sliderPos.x - bounds.left)/200.0*255.0;
-    int newRGB = int(RGB_value);
-    sf::FloatRect tmp = menu.getLocalBounds();
-    colorsvec.clear();
-    colors[0].position = sf::Vector2f( tmp.left + tmp.width, tmp.top );
-    colors[1].position = sf::Vector2f( tmp.left + tmp.width, tmp.top + displayy ); 
-    colors[2].position = sf::Vector2f( displayx, displayy );
-    colors[3].position = sf::Vector2f( displayx, 0.0 );
-    // 0 = TL
-    // 1 = TR
-    // 2 = BR
-    // 3 = BL
-    if(newRGB>=0){ 
-      switch(i){
-      case 0 :
-	R = newRGB;
-	colors[0].color = sf::Color(R,0.0,0.0);
-	colors[1].color = sf::Color(0.0,G,0.0);
-	colors[2].color = sf::Color(0.0,0.0,B);
-	colors[3].color = sf::Color(R2,0.0,0.0);
-	break;
-      case 1 :
-	G = newRGB;
-	colors[0].color = sf::Color(R,0.0,0.0);
-	colors[1].color = sf::Color(0.0,G,0.0);
-	colors[2].color = sf::Color(0.0,0.0,B);
-	colors[3].color = sf::Color(R2,0.0,0.0);
-	break;
-      case 2 : 
-	B = newRGB;
-	colors[0].color = sf::Color(R,0.0,0.0);
-	colors[1].color = sf::Color(0.0,G,0.0);
-	colors[2].color = sf::Color(0.0,0.0,B);
-	colors[3].color = sf::Color(R2,0.0,0.0);
-	break;
-      case 3 : 
-	R2 = newRGB;
-	colors[0].color = sf::Color(R,0.0,0.0);
-	colors[1].color = sf::Color(0.0,G,0.0);
-	colors[2].color = sf::Color(0.0,0.0,B);
-	colors[3].color = sf::Color(R2,0.0,0.0);
-	break;
-      default : 
-      break;
-      }
-      colorsvec.push_back( colors );
-    }
-    
-  }
+void Menu::MakeBoarder(float x, float y, float d, sf::Color color) {
+  // Make a "Ceiling"
+  sf::Vector2f ceiling_size( x, d );
+  ceiling.setSize( ceiling_size );
+  ceiling.setOrigin( x/2.0, d/2.0 );
+  ceiling.setFillColor( color );
+  ceiling.setPosition( x/2.0, d/2.0);
+  // Make a bottom
+  bottom.setSize( ceiling_size );
+  bottom.setOrigin( x/2.0, d/2.0 );
+  bottom.setFillColor( color );
+  bottom.setPosition( x/2.0, y - d/2.0);
+  // Make a Left / Right side
+  left.setSize( sf::Vector2f( d, y - 2.0*d) );
+  left.setOrigin( d/2.0, (y - 2.0*d)/2.0 );
+  left.setFillColor( color );
+  left.setPosition( d/2.0, y/2.0 );
+  right.setSize( sf::Vector2f( d, y - 2.0*d) );
+  right.setOrigin( d/2.0, (y - 2.0*d)/2.0 );
+  right.setFillColor( color );
+  right.setPosition( x - d/2.0, y/2.0 );
+  boarder.push_back( ceiling );
+  boarder.push_back( bottom );
+  boarder.push_back( left );
+  boarder.push_back( right );
 }
